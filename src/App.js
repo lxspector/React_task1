3e2d2d2a64784656807f66f3eda6ed7c295f6c1e
lxspector <lxspector@gmail.com>
2023-09-08 23:14:14 +0200

    Первый вариант выполнения кода. Часть до <p> с датой - декларативная, при вызове <p> - императивная

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 export const App = () => {
-	//Императив
-	const currentYear = new Date().getFullYear();
-
 	return (
-		//Декларатив
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
@@ -21,7 +17,9 @@ export const App = () => {
 				>
 					Learn React
 				</a>
-				<p>{currentYear}</p>
+				{/* Декларативный стиль*/}
+				<p>{new Date().getFullYear()}</p>
+				{/* Императивный стиль*/}
 			</header>
 		</div>
 	);
