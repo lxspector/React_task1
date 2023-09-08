5ffd8e55a4218b3ef2766792f1bf9509c5299442
lxspector <lxspector@gmail.com>
2023-09-08 23:20:16 +0200

    App без JSX по Бабелю

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { jsx as _jsx } from 'react/jsx-runtime';
+import { jsxs as _jsxs } from 'react/jsx-runtime';
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				{/* Декларативный стиль*/}
-				<p>{new Date().getFullYear()}</p>
-				{/* Императивный стиль*/}
-			</header>
-		</div>
-	);
+	return /*#__PURE__*/ _jsx('div', {
+		className: 'App',
+		children: /*#__PURE__*/ _jsxs('header', {
+			className: 'App-header',
+			children: [
+				/*#__PURE__*/ _jsx('img', {
+					src: logo,
+					className: 'App-logo',
+					alt: 'logo',
+				}),
+				/*#__PURE__*/ _jsxs('p', {
+					children: [
+						'Edit ',
+						/*#__PURE__*/ _jsx('code', {
+							children: 'src/App.js',
+						}),
+						' and save to reload.',
+					],
+				}),
+				/*#__PURE__*/ _jsx('a', {
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+					children: 'Learn React',
+				}),
+				/*#__PURE__*/ _jsx('p', {
+					children: new Date().getFullYear(),
+				}),
+			],
+		}),
+	});
 };
-
 export default App;
